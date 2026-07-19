cf62f940c283314e523cf1765d056a5ec0d11b2d
zane <39070793+zaneenders@users.noreply.github.com>
2026-07-18 23:49:18 -0600

    triangle

--- Sources/HelloTriangle/Renderer.swift
+++ Sources/HelloTriangle/Renderer.swift
@@ -52,7 +52,7 @@ final class Renderer: NSObject, MTKViewDelegate {
         let verties = [
             Vertex(positions: [0.0, 0.8], color: [1, 0, 0]),
             Vertex(positions: [-0.8, -0.8], color: [0, 1, 0]),
-            Vertex(positions: [0.8, 0.0], color: [0, 0, 1]),
+            Vertex(positions: [0.8, -0.8], color: [0, 0, 1]),
         ]
         guard
             let vertexBuffer = device.makeBuffer(
